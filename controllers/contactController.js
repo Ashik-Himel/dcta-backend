2b83952fbd318609645361e9f92d1fc40f3e8cc1
Ashik-Himel <ashikujjamanhimel@gmail.com>
2025-04-27 21:13:39 +0600

    create contact API created

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,51 @@
+/* eslint-disable object-curly-newline */
 import { getDB } from '../configs/db.js';
+import sendEmail from '../configs/email.js';
+import { adminEmail } from '../configs/variables.js';
+
+export const createContact = async (req, res, next) => {
+  try {
+    const db = getDB();
+    const { name, email, subject, message } = req.body;
+
+    if (!name || !email || !subject || !message) {
+      return res.status(400).json({
+        ok: false,
+        message: 'All fields are required',
+      });
+    }
+
+    const newContact = {
+      name,
+      email,
+      message,
+      status: 'New',
+      date: new Date(),
+    };
+
+    await db.collection('contacts').insertOne(newContact);
+
+    await sendEmail({
+      to: adminEmail,
+      subject: "Contact from DCTA's website",
+      html: `
+            <h3>Here is the contact details:</h3>
+            <p><span style:"font-weight:bold">Name:</span> ${name}</p>
+            <p><span style:"font-weight:bold">Email:</span> ${email}</p>
+            <p><span style:"font-weight:bold">Subject:</span> ${subject}</p>
+            <p style:"font-weight:bold">Message:</p>
+            <p>${message}</p>
+          `,
+    });
+
+    return res.status(201).json({
+      ok: true,
+      message: 'Message sent successfully',
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 export const getRecentContacts = async (req, res, next) => {
   try {
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,15 @@
 /* eslint-disable object-curly-newline */
 import express from 'express';
-import { getAllContacts, getRecentContacts } from '../controllers/contactController.js';
+import {
+  createContact,
+  getAllContacts,
+  getRecentContacts,
+} from '../controllers/contactController.js';
 import { authorizeAdmin, authorizeUser } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.post('/', createContact);
 router.get('/recent-contacts', authorizeUser, authorizeAdmin, getRecentContacts);
 router.get('/all-contacts', authorizeUser, authorizeAdmin, getAllContacts);
 
